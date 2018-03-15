d17c198006029adfd8b52d7603415ce316c7f181
irina <ialeksandrovich@bk.ru>
2018-03-15 19:59:31 +0600

    updetes

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// main-menu.js
+
+var menuButton = document.querySelector(".page-header__toggle");
+var menu = document.querySelector(".page-header__nav");
+
+menuButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.toggle("page-header__nav--closed");
+});
+
+// modal.js
+
+var feedbackForm = document.querySelector(".feedback__form");
+
+if (feedbackForm) {
+  var firstName = feedbackForm.querySelector("[name=first-name]");
+  var secondName = feedbackForm.querySelector("[name=second-name]");
+  var tel = feedbackForm.querySelector("[name=tel]");
+  var email = feedbackForm.querySelector("[name=email]");
+  var modalFailure = document.querySelector(".modal-failure");
+  var modalSuccess = document.querySelector(".modal-success");
+  var modalFailureButton = document.querySelector(".modal-failure__button");
+  var modalSuccessButton = document.querySelector(".modal-success__button");
+
+  feedbackForm.addEventListener("submit", function(event) {
+    if (!firstName.value || !secondName.value || !tel.value || !email.value) {
+      event.preventDefault();
+      modalFailure.classList.remove("display-none");
+    } else {
+      event.preventDefault();
+      modalSuccess.classList.remove("display-none");
+    }
+  });
+
+
+  modalFailureButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalFailure.classList.add("display-none");
+  });
+
+
+  modalSuccessButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalSuccess.classList.add("display-none");
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (!modalFailure.classList.contains("display-none")) {
+        modalFailure.classList.add("display-none");
+      }
+      if (!modalSuccess.classList.contains("display-none")) {
+        modalSuccess.classList.add("display-none");
+      }
+    }
+  });
+}
--- src/js/main-menu.js
+++
@@ -1,9 +0,0 @@
-// main-menu.js
-
-var menuButton = document.querySelector(".page-header__toggle");
-var menu = document.querySelector(".page-header__nav");
-
-menuButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  menu.classList.toggle("page-header__nav--closed");
-});
